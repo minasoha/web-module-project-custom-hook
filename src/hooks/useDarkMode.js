f6f7ac5c59abb1d826ccbb140131cee6fa01fb09
minasoha <minasoha@yahoo.com>
2021-10-27 18:16:06 -0400

    finished everything

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,7 @@
+import { useLocalStorage } from "./useLocalStorage";
+
+const useDarkMode = (initialValue) => {
+  const [someValue, setSomeValue] = useLocalStorage(initialValue);
+  return [someValue, setSomeValue];
+};
+export default useDarkMode;
